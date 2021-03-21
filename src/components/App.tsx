031f4368e9ae7aa525417405d4c99b6bd32d1dae
Zeztron <pateltharsh@gmail.com>
2021-03-21 09:01:28 -0400

    refactored reusable components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,81 +1,9 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import { fetchPlugin } from '../plugins/fetch-plugin';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
-
-import CodeEditor from './CodeEditor';
+import CodeCell from './CodeCell';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-  const [input, setInput] = useState('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    if (!ref.current) return;
-
-    iframe.current.srcdoc = html; // Reset the iframe contents
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', event => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              const root = document.getElementById('root');
-              root.innerHTML = '<div style="color: #d40511;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
-
   return (
     <div>
-      <CodeEditor initialValue='' onChange={(value) => setInput(value)} />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe
-        ref={iframe}
-        sandbox='allow-scripts'
-        title='preview'
-        srcDoc={html}
-      />
+      <CodeCell />
     </div>
   );
 };
